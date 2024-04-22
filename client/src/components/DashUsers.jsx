b752c4dd6f5e10a7f97c02dad3548d81da16eb03
Gloire Beya <gloweb78@gmail.com>
2024-04-22 22:21:57 +0200

    Add delete user functionality to the admin user

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -31,7 +31,22 @@ export default function DashUsers() {
       }
    }, [currentUser._id])
 
-   const handleDeleteUser = () => { }
+   const handleDeleteUser = async () => {
+      try {
+         const res = await fetch(`/api/user/delete/${userIdToDelete}`, {
+            method: 'DELETE'
+         })
+         const data = await res.json()
+         if (res.ok) {
+            setUsers((prev) => prev.filter((user) => user._id !== userIdToDelete))
+            setShowModal(false)
+         } else {
+            console.log(data.message);
+         }
+      } catch (error) {
+         console.error(error.message);
+      }
+   }
 
    const handleShowMore = async () => {
       const startIndex = users.length
